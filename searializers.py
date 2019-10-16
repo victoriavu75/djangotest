ef11312017cb645f81fc896ec76809675bffcb25
Victoria Vu <victoriavu75@gmail.com>
2019-10-16 00:11:36 -0700

    Delete searializers.py

--- searializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from . models import volunteers
-
-class volunteersSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = volunteers
-        fields ='__all__'
