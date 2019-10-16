1c73587f3d9947ba37c751b5ef2993194602bf3b
Victoria Vu <victoriavu75@gmail.com>
2019-10-16 00:00:48 -0700

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from . models import volunteers
+
+admin.site.register(volunteers)
+# Register your models here.
---
+++ apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Inf117ApiConfig(AppConfig):
+    name = 'inf117api'
